f715a96db7b1891d197ce67d4b3eb4889539af3b
MIAIONE@YEAH.NET <MIAIONE@YEAH.NET>
2022-01-25 23:33:47 +0800

    添加项目文件。

---
+++ Skeliya/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SVM
+{
+    /// <summary>
+    /// 主运行类
+    /// </summary>
+    public class SkeliyaHost
+    {
+        /// <summary>
+        /// 应用程序入口点
+        /// </summary>
+        public static void Main()
+        {
+
+        }
+    }
+}
